d7ff2473098177d14cc8d65d97ae466413ec9749
Tang Tou <tyu4520@gmail.com>
2021-03-31 18:08:15 +0800

    判斷會員測試

--- lib/user.rb
+++ lib/user.rb
@@ -1,6 +1,16 @@
 class User
-    def member
-        @member = member
-    end
+  attr_reader :is_login, :is_member
+
+  def initialize
+    @is_login = false
+    @is_member = false
+  end
+
+  def login
+    @is_login = true
+    @is_member = true
+  end
+
+ 
 
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,15 +1,26 @@
 require './lib/user'
 
+# 會員
+# 登入
+# 加入會員
+# 忘記密碼
+
+
+# 非會員
+# 單次租車
+
 RSpec.describe User do
   # pending '試著動手寫一些測試吧！'
 
-  # it "會員" do
-  #   user = User.new
-  #   expect(user.member).to be true
-  # end
+  it "會員登入" do
+    user = User.new
+    user.login
+    expect(user.is_login).to be true
+    expect(user.is_member).to be true
+  end
 
-  # it "非會員" do
-  #   user = User.new
-  #   expect(user.member).to be false
-  # end
+  it "非會員不會登入" do
+    user = User.new
+    expect(user.is_member).to be false
+  end
 end
